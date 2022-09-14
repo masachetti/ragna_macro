7b7fa837b80bcb5d231bfe55d2d43eeb6306cbc0
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-14 17:57:45 -0400

    Create methods to access the new addresses

--- core/memory_reader.py
+++ core/memory_reader.py
@@ -3,7 +3,9 @@ from threading import Lock
 
 import utils
 from core.client_handler import ClientHandler
+
 uint = utils.parse_bytes_to_uint32
+
 
 class MemoryReader(object):
     _instance = None
@@ -20,7 +22,15 @@ class MemoryReader(object):
         self.addresses = parse_addresses(load_addresses())
         self.client = ClientHandler()
         self.get_current_hp = self._create_reader_function('current-hp', 4, uint)
+        self.get_max_hp = self._create_reader_function('max-hp', 4, uint)
+        self.get_current_sp = self._create_reader_function('current-sp', 4, uint)
+        self.get_max_sp = self._create_reader_function('max-sp', 4, uint)
+        self.get_current_weight = self._create_reader_function('weight', 4, uint)
+        self.get_max_weight = self._create_reader_function('max-weight', 4, uint)
         self.get_current_map_name = self._create_reader_function('current-map-name', 25, extract_map_name)
+        self.get_coordinate_x = self._create_reader_function('coordinate-x', 4, uint)
+        self.get_coordinate_y = self._create_reader_function('coordinate-y', 4, uint)
+        self.get_buffs = self._create_reader_function('coordinate-y', 4 * 100, extract_buffs)
 
     def _create_reader_function(self, address_name, memory_len, value_parser):
         def reader():
@@ -34,13 +44,18 @@ class MemoryReader(object):
                 return parsed_value
             else:
                 raise SyntaxError(f"Incorrect address for '{address_name}', check the file addresses.json.")
+
         return reader
+
+
+def extract_buffs(buffs_bytes):
+    return None
 
 
 def extract_map_name(map_name_bytes):
     return utils.parse_bytes_to_string(map_name_bytes.split(b'\00')[0])
 
-    
+
 def parse_addresses(addresses):
     return dict(map(lambda x: (x[0], utils.parse_hex_string_to_int(x[1])), addresses.items()))
 
@@ -50,3 +65,16 @@ def load_addresses():
     with open('../addresses.json') as json_file:
         content = json.load(json_file)
     return content
+
+
+if __name__ == '__main__':
+   mem = MemoryReader()
+   print(f"Current Hp: {mem.get_current_hp()}")
+   print(f"Max Hp: {mem.get_max_hp()}")
+   print(f"Current Sp: {mem.get_current_sp()}")
+   print(f"Max Sp: {mem.get_max_sp()}")
+   print(f"Current W: {mem.get_current_weight()}")
+   print(f"Max W: {mem.get_max_weight()}")
+   print(f"Current Map: {mem.get_current_map_name()}")
+   print(f"X: {mem.get_coordinate_x()}")
+   print(f"Y: {mem.get_coordinate_y()}")
