63135e1147ab21e60e5968b5e036b26a41cb05e1
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-14 17:22:13 -0400

    Create hotkey_listener.py

---
+++ macro_system_test/hotkey_listener.py
@@ -0,0 +1,47 @@
+from pynput import keyboard
+
+
+class HotkeyListener:
+    # Singleton
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(HotkeyListener, cls).__new__(cls)
+            cls._instance._setup()
+        return cls._instance
+
+    def _setup(self):
+        self.listener: keyboard.Listener = None
+        self.pressed_keys = list()
+        self._observers = list()
+
+    def start(self):
+        if self.listener is None:
+            self.listener = keyboard.Listener(
+                on_press=self.on_press,
+                on_release=self.on_release)
+            self.listener.start()
+
+    def on_press(self, key):
+        if key not in self.pressed_keys:
+            self.pressed_keys.append(key)
+            self.notify()
+
+    def on_release(self, key):
+        if key in self.pressed_keys:
+            self.pressed_keys.remove(key)
+            self.notify()
+
+    def notify(self):
+        for observer_callback in self._observers:
+            observer_callback(self.pressed_keys)
+
+    def attach_observer(self, observer_callback):
+        self._observers.append(observer_callback)
+
+
+if __name__ == '__main__':
+    watcher = HotkeyListener()
+    watcher.attach_observer(lambda x: print(f"Pressed keys: {x}"))
+    watcher.start()
