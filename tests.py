fa34dcd956f10621ec13160f271dec9cda5f0080
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-14 17:23:19 -0400

    Create some tests

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
 import time
 
+import pynput
 import win32con
 
-from client_handler import ClientHandler
+from core.client_handler import ClientHandler
+from macro_manager import MacroManager
+from macro_pool_handler import MacroPoolHandler
+from macros.macro_test import MacroTest
 
 
 def test_usar_skill():
@@ -16,5 +20,25 @@ def test_usar_skill():
     time.sleep(0.05)
     client.send_mouse_lbutton_up()
 
+
+def test_macro_pool_handler():
+    macro_test1 = MacroTest(pynput.keyboard.Key.f2, 2)
+    macro_test2 = MacroTest(pynput.keyboard.Key.f4, "Alguma coisa aqui", 2000)
+    pool = MacroPoolHandler([macro_test1, macro_test2])
+    print(pool.macro_list)
+    print(pool._listeners)
+    print(pool._enabled_macros)
+    pool.start()
+
+
+def test_macro_manager():
+    macro_test1 = MacroTest(pynput.keyboard.Key.f2, 2)
+    macro_test2 = MacroTest(pynput.keyboard.Key.f4, "Alguma coisa aqui", 2000)
+    pool = MacroManager([macro_test1, macro_test2])
+    print(pool.macro_list)
+    print(pool._listeners)
+    pool._listeners[0].join()
+
+
 if __name__ == '__main__':
-    test_usar_skill()
+    test_macro_manager()
