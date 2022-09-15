59ff47deba6a8f3980a328bb307b0aa2d677b174
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-14 21:51:07 -0400

    Add an auto pot macro

---
+++ macros/auto_pot.py
@@ -0,0 +1,24 @@
+from core import macro_methods
+from pynput import keyboard
+
+from core.memory_reader import MemoryReader
+from models.macro import Macro, TriggerType
+
+
+class AutoPot(Macro):
+    def __init__(self, key: keyboard.Key, delay: int = 500, percent_hp: int = 80):
+        self.percent_hp = percent_hp
+        self.memory_reader = MemoryReader()
+        self.pot_key = key
+        super().__init__(trigger_type=TriggerType.ALWAYS_TRIGGERED,
+                         delay=delay)
+
+    def action(self):
+        macro_methods.click_key(self.pot_key)
+
+    def action_condition_checker(self):
+        max_hp = self.memory_reader.get_max_hp()
+        current_hp = self.memory_reader.get_current_hp()
+        current_hp_percent = (current_hp / max_hp) * 100
+        return current_hp_percent <= self.percent_hp
+
--- profiles/trovador.py
+++ profiles/trovador.py
@@ -1,6 +1,8 @@
+from macros.auto_pot import AutoPot
 from macros.skill_spam import SkillSpam
 from pynput import keyboard
 
 macros = [
-    SkillSpam(keyboard.Key.f1, 10)
+    SkillSpam(keyboard.Key.f1, 10),
+    AutoPot(keyboard.Key.f9)
 ]
