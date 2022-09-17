30b787a8ff2f7029ce98326536ef32919a2b376f
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-17 14:34:17 -0400

    Add 'has buff' method to the memory reader
    - Added 'has buff' method
    - Now args and kwargs parameters are passed to the reader method

--- core/memory_reader.py
+++ core/memory_reader.py
@@ -1,8 +1,8 @@
 import json
 from threading import Lock
-
 from utils import utils
 from core.client_handler import ClientHandler
+from resources.buffs_code import Buffs
 
 uint = utils.parse_bytes_to_uint32
 
@@ -30,17 +30,17 @@ class MemoryReader(object):
         self.get_current_map_name = self._create_reader_function('current-map-name', 25, extract_map_name)
         self.get_coordinate_x = self._create_reader_function('coordinate-x', 4, uint)
         self.get_coordinate_y = self._create_reader_function('coordinate-y', 4, uint)
-        self.get_buffs = self._create_reader_function('coordinate-y', 4 * 100, extract_buffs)
+        self.has_buff = self._create_reader_function('buffs-array', 4 * 100, has_buff)
 
     def _create_reader_function(self, address_name, memory_len, value_parser):
-        def reader():
+        def reader(*args, **kwargs):
             if address_name in self.addresses:
                 address = self.addresses[address_name]
             else:
                 raise ValueError(f"Can't find '{address_name}' in the addresses list, check the file addresses.json.")
             if address:
                 memory_value = self.client.read_address(address, memory_len)
-                parsed_value = value_parser(memory_value)
+                parsed_value = value_parser(memory_value, *args, **kwargs)
                 return parsed_value
             else:
                 raise SyntaxError(f"Incorrect address for '{address_name}', check the file addresses.json.")
@@ -48,8 +48,14 @@ class MemoryReader(object):
         return reader
 
 
-def extract_buffs(buffs_bytes):
-    return None
+def has_buff(buffs_bytes, buff_code_to_find):
+    cursor = 0
+    while cursor < len(buffs_bytes):
+        buff_code = uint(buffs_bytes[cursor:cursor+4])
+        cursor += 4
+        if buff_code == buff_code_to_find:
+            return True
+    return False
 
 
 def extract_map_name(map_name_bytes):
@@ -68,13 +74,13 @@ def load_addresses():
 
 
 if __name__ == '__main__':
-   mem = MemoryReader()
-   print(f"Current Hp: {mem.get_current_hp()}")
-   print(f"Max Hp: {mem.get_max_hp()}")
-   print(f"Current Sp: {mem.get_current_sp()}")
-   print(f"Max Sp: {mem.get_max_sp()}")
-   print(f"Current W: {mem.get_current_weight()}")
-   print(f"Max W: {mem.get_max_weight()}")
-   print(f"Current Map: {mem.get_current_map_name()}")
-   print(f"X: {mem.get_coordinate_x()}")
-   print(f"Y: {mem.get_coordinate_y()}")
+    mem = MemoryReader()
+    print(f"Current Hp: {mem.get_current_hp()}")
+    print(f"Max Hp: {mem.get_max_hp()}")
+    print(f"Current Sp: {mem.get_current_sp()}")
+    print(f"Max Sp: {mem.get_max_sp()}")
+    print(f"Current W: {mem.get_current_weight()}")
+    print(f"Max W: {mem.get_max_weight()}")
+    print(f"Current Map: {mem.get_current_map_name()}")
+    print(f"X: {mem.get_coordinate_x()}")
+    print(f"Y: {mem.get_coordinate_y()}")
