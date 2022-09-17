596872b5237deea407b005fe77ff68aa82511a66
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-17 14:35:23 -0400

    Add auto buffs macros to the 'trovador' profile

--- profiles/trovador.py
+++ profiles/trovador.py
@@ -1,10 +1,15 @@
+from macros.auto_buff import AutoBuff
 from macros.auto_pot import AutoPot
 from macros.farming import RainstormFarming
 from macros.skill_spam import SkillSpam
 from pynput import keyboard
 
+from resources.buffs_code import Buffs
+
 macros = [
     SkillSpam(keyboard.Key.f1, 10),
     AutoPot(keyboard.Key.f9),
-    RainstormFarming(keyboard.Key.f1, keyboard.Key.f3, keyboard.Key.end, 'abyss_03')
+    RainstormFarming(keyboard.Key.f1, keyboard.Key.f3, keyboard.Key.end, 'abyss_03'),
+    AutoBuff(keyboard.Key.f2, Buffs.Concentration),
+    AutoBuff(keyboard.Key.f4, Buffs.PoemOfBragi),
 ]
