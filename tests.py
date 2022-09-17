d3548ffea9f41edaf8f99879ee51185dcdbbc173
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-17 14:35:28 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,13 +1,11 @@
 import time
-
-import pynput
 import win32con
 
 from core.client_handler import ClientHandler
-
 from core.macros_monitor import MacrosMonitor
-from macros.macro_test import MacroTest
+from core.memory_reader import MemoryReader
 from models.macro import Macro
+from resources.buffs_code import Buffs
 
 
 def test_usar_skill():
@@ -21,13 +19,21 @@ def test_usar_skill():
     time.sleep(0.05)
     client.send_mouse_lbutton_up()
 
+
 def test_macro_monitor():
     macro1 = Macro(name="Macro1")
+    macro1.running = True
     macro2 = Macro(name="Macro2")
     macro3 = Macro(name="Macro3", hotkey='F1')
 
     monitor = MacrosMonitor([macro1, macro2, macro3])
     monitor.start()
 
+
+def test_buff_reader():
+    mem_reader = MemoryReader()
+    print(mem_reader.has_buff(Buffs.CartBoost))
+
+
 if __name__ == '__main__':
-    test_macro_monitor()
+    test_buff_reader()
