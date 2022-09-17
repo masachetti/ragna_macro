818159ff6b8946b29683a832ef36865f3a5e4aaf
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-16 22:23:15 -0400

    Improve macro monitor display

--- core/macros_monitor.py
+++ core/macros_monitor.py
@@ -10,21 +10,53 @@ Colocar a hotkey entre parentenses depois do nome da macro
 """
 import curses
 from dataclasses import dataclass
-from typing import List
+from typing import List, Union, Tuple
 
 from models.macro import Macro
 
 ABORT_KEYS = [curses.KEY_END]
+COLOR_TRUE_CONDITION_TIP = 1
+COLOR_FALSE_CONDITION_TIP = 2
+COLOR_TRUE_CONDITION = 3
+COLOR_FALSE_CONDITION = 4
+
+
+def config_curses():
+    try:
+        # use the default colors of the terminal
+        # curses.use_default_colors()
+        # hide the cursor
+        curses.curs_set(0)
+        curses.init_pair(COLOR_TRUE_CONDITION_TIP, curses.COLOR_WHITE, curses.COLOR_GREEN)
+        curses.init_pair(COLOR_FALSE_CONDITION_TIP, curses.COLOR_WHITE, curses.COLOR_RED)
+        curses.init_pair(COLOR_TRUE_CONDITION, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(COLOR_FALSE_CONDITION, curses.COLOR_RED, curses.COLOR_BLACK)
+    except:
+        # Curses failed to initialize color support, eg. when TERM=vt100
+        curses.initscr()
+
+
+def create_macro_repr(macro: Macro) -> str:
+    hk = macro.hotkey
+    return f"{macro.name} ({hk if hk else '-'})"
+
 
 @dataclass
 class MacrosMonitor:
     macros: List[Macro]
 
+    def get_triggered_macros(self):
+        return [(create_macro_repr(macro), 4 - int(macro.action_condition)) for macro in self.macros if macro.running]
+
+    def get_not_triggered_macros(self):
+        return [create_macro_repr(macro) for macro in self.macros if not macro.running]
+
     def get_lines(self):
-        lines: List[str] = []
-        for macro in self.macros:
-            hk = macro.hotkey
-            lines.append(f"{macro.name} ({hk if hk else '-'})")
+        lines: List[Union[str, Tuple[str, int]]] = ["--- Triggered Macros ---"]
+        lines.extend(self.get_triggered_macros())
+        lines.append('')
+        lines.append("--- Not Triggered Macros ---")
+        lines.extend(self.get_not_triggered_macros())
         return lines
 
     def draw(self, screen):
@@ -34,12 +66,20 @@ class MacrosMonitor:
         max_y, max_x = screen.getmaxyx()
         max_rows = max_y - y  # the max rows we can draw
 
-        lines = self.get_lines()
+        true_condition_str = "CONDITION = True"
+        screen.addstr(y, x, true_condition_str, curses.color_pair(COLOR_TRUE_CONDITION_TIP))
+        screen.addstr(y, x + len(true_condition_str) + 5, "CONDITION = False",
+                      curses.color_pair(COLOR_FALSE_CONDITION_TIP))
+        y += 2
 
-        lines_to_draw = lines[:max_rows]
+        lines = self.get_lines()
+        lines_to_draw = lines[:max_rows - 2]
 
         for line in lines_to_draw:
-            screen.addnstr(y, x, line, max_x - 2)
+            if isinstance(line, str):
+                screen.addnstr(y, x, line, max_x - 2)
+            elif isinstance(line, tuple):
+                screen.addnstr(y, x, line[0], max_x - 2, curses.color_pair(line[1]))
             y += 1
 
         screen.refresh()
@@ -51,18 +91,8 @@ class MacrosMonitor:
             if c in ABORT_KEYS:
                 break
 
-    def config_curses(self):
-        try:
-            # use the default colors of the terminal
-            curses.use_default_colors()
-            # hide the cursor
-            curses.curs_set(0)
-        except:
-            # Curses failed to initialize color support, eg. when TERM=vt100
-            curses.initscr()
-
     def _start(self, screen):
-        self.config_curses()
+        config_curses()
         self.run_loop(screen)
 
     def start(self):
