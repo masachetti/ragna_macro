69d2d9e2147ee985940cea4858c537ba1d5f79a1
masachetti <50852444+masachetti@users.noreply.github.com>
2022-09-20 09:59:34 -0400

    Convert 'biolo' profile to dictionary

--- profiles/biolo.py
+++ profiles/biolo.py
@@ -1,12 +1,15 @@
 from pynput.keyboard import Key
 
 from macros.auto_buff import AutoBuff
+from macros.click_spam import ClickSpam
 from macros.hp_auto_pot import HpAutoPot
 from macros.skill_spam import SkillSpam
 from resources.buffs_code import Buffs
 
-macros = [
-    SkillSpam(Key.f1, 10),
-    HpAutoPot(Key.f9, percent_hp=60),
-    AutoBuff(Key.f2, Buffs.CartBoost),
-]
+macros = {
+    "Skill Spam [F1]": SkillSpam(Key.f1, 10),
+    "Auto-Pot | Hp (F9)": HpAutoPot(Key.f9, percent_hp=60),
+    "Auto-Buff | Cart Boost (F)": AutoBuff(Key.f2, Buffs.CartBoost),
+    "Auto-Clicker [Home]": ClickSpam(Key.home)
+}
+
